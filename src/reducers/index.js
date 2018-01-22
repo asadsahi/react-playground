531a0de8b228956e9b20f623c0d8e70c5f48b214
Asad <asadazmat@gmail.com>
2018-01-22 17:40:34 +0000

    examples reducers refactored

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,42 +3,14 @@ import { routerReducer } from 'react-router-redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 
 // App Reducers
-
 // Examples reducers
-// Shopping cart reducer
-import {
-  cart,
-  products
-} from '../containers/examples/components/redux/shopping-cart/reducers';
-// Counter example reducers
-import counter from '../containers/examples/components/redux/counter/counter';
-// Currency convertor example reducers
-import amount from '../containers/examples/components/redux/currency-convertor/reducers/amount';
-import error from '../containers/examples/components/redux/currency-convertor/reducers/error';
-// Course example reducers
-import courses from '../containers/examples/components/redux/course-management/reducers/courseReducer';
-import authors from '../containers/examples/components/redux/course-management/reducers/authorReducer';
-import ajaxCallsInProgress from '../containers/examples/components/redux/course-management/reducers/ajaxStatusReducer';
-// Todos
-import {
-  todos,
-  visibilityFilter
-} from '../containers/examples/components/redux/Todo';
+import * as exampleReducers from '../containers/examples/reducers';
 
 const rootReducer = combineReducers({
   //---------------
   form: reduxFormReducer, // mounted under "form"
   router: routerReducer,
-  counter,
-  amount,
-  error,
-  courses,
-  authors,
-  ajaxCallsInProgress,
-  todos,
-  visibilityFilter,
-  cart,
-  products
+  ...exampleReducers
 });
 
 export { rootReducer };
