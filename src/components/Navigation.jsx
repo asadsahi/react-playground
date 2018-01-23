a6a88872251c981af30247877ab6bec309ec4993
Asad <asadazmat@gmail.com>
2018-01-23 17:56:46 +0000

    nav fixes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink as RouterNavLink } from 'react-router-dom';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavLink,
-  NavItem
-} from 'reactstrap';
-
+import { NavLink } from 'react-router-dom';
 import { logout } from '../services';
 
 export class Navigation extends Component {
@@ -25,74 +15,85 @@ export class Navigation extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar color="faded" light expand="md">
-          <NavbarBrand tag={RouterNavLink} to="/">
-            React playground
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag={RouterNavLink} to="/" activeClassName="active">
-                  Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  tag={RouterNavLink}
-                  to="/examples"
-                  activeClassName="active"
-                >
-                  Examples
-                </NavLink>
-              </NavItem>
-              {this.props.authed
-                ? [
-                    <NavItem key="profile">
-                      <NavLink
-                        tag={RouterNavLink}
-                        to="/profile"
-                        activeClassName="active"
-                      >
-                        {this.props.user.email}
-                      </NavLink>
-                    </NavItem>,
-                    <NavItem key="logout">
-                      <NavLink
-                        href="#"
-                        onClick={() => {
-                          logout();
-                        }}
-                      >
-                        Logout
-                      </NavLink>
-                    </NavItem>
-                  ]
-                : [
-                    <NavItem key="login">
-                      <NavLink
-                        tag={RouterNavLink}
-                        to="/login"
-                        activeClassName="active"
-                      >
-                        Login
-                      </NavLink>
-                    </NavItem>,
-                    <NavItem key="register">
-                      <NavLink
-                        tag={RouterNavLink}
-                        to="/register"
-                        activeClassName="active"
-                      >
-                        Register
-                      </NavLink>
-                    </NavItem>
-                  ]}
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink className="navbar-brand" to="/">
+          React playground
+        </NavLink>
+        <button
+          onClick={this.toggle}
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/examples"
+                activeClassName="active"
+              >
+                Examples
+              </NavLink>
+            </li>
+            {this.props.authed
+              ? [
+                  <li className="nav-item" key="profile">
+                    <NavLink
+                      className="nav-link"
+                      to="/profile"
+                      activeClassName="active"
+                    >
+                      {this.props.user.email}
+                    </NavLink>
+                  </li>,
+                  <li className="nav-item" key="logout">
+                    <a
+                      className="nav-link"
+                      href="#"
+                      onClick={() => {
+                        logout();
+                      }}
+                    >
+                      Logout
+                    </a>
+                  </li>
+                ]
+              : [
+                  <li className="nav-item" key="login">
+                    <NavLink
+                      className="nav-link"
+                      to="/login"
+                      activeClassName="active"
+                    >
+                      Login
+                    </NavLink>
+                  </li>,
+                  <li className="nav-item" key="register">
+                    <NavLink
+                      className="nav-link"
+                      to="/register"
+                      activeClassName="active"
+                    >
+                      Register
+                    </NavLink>
+                  </li>
+                ]}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
